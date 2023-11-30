f18c407d7678477876254596cc115f089d4ca248
André Rodrigues <andre.rodrigues98@outlook.com>
2023-11-29 21:14:48 -0300

    feat: improving find example

--- JavaScript/array-find.js
+++ JavaScript/array-find.js
@@ -1,7 +1,11 @@
 const array = [1, 2, 3, 4, 50, 6, 50, 8, 9, 10];
 
-const compare = (number) => number > 20;
-const index = array.find(compare);
-
-console.log(index);
+const compareGreaterThan20 = (number) => number > 20;
+const value = array.find(compareGreaterThan20);
+console.log(value);
 // output: 50
+
+const compareGreaterThan100 = (number) => number > 100;
+const notfound = array.find(compareGreaterThan100);
+console.log(notfound);
+// output: undefined
