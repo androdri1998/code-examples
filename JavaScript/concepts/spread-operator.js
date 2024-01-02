4b30aa599a58150e4e7465568f7f8d758384901b
André Rodrigues <andre.rodrigues98@outlook.com>
2024-01-02 08:51:22 -0300

    fix: removing rest syntax

--- JavaScript/concepts/spread-operator.js
+++ JavaScript/concepts/spread-operator.js
@@ -5,18 +5,14 @@ const newArray = [...array, 5];
 console.log(newArray);
 // output: [ 1, 2, 3, 4, 5 ]
 
-const [numberOne, numberTwo, ...arrayRemainNumbers] = newArray;
+const [numberOne, numberTwo] = newArray;
 console.log(numberOne, numberTwo);
 // output: 1 2
-console.log(arrayRemainNumbers);
-// output: [ 3, 4, 5 ]
 
 const newObject = { ...object, five: 5 };
 console.log(newObject);
 // output: { one: 1, two: 2, three: 3, four: 4, five: 5 }
 
-const { one, two, ...remainNumbers } = newObject;
+const { one, two } = newObject;
 console.log(one, two);
 // output: 1 2
-console.log(remainNumbers);
-// output: { three: 3, four: 4, five: 5 }
