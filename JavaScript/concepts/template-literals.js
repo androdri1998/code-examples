fb4b9d69eeb40d23c9989cab7202ae1b66852ecd
André Rodrigues <andre.rodrigues98@outlook.com>
2024-01-02 20:30:38 -0300

    refactor: adding generic to template literals

--- JavaScript/concepts/template-literals.js
+++ JavaScript/concepts/template-literals.js
@@ -1,16 +1,16 @@
 const person = {
-  name: "John Doe",
+  description: "people",
   age: 21,
 };
 
 const template = `
-  ${person.name}
+  some ${person.description}
             has 
                             ${person.age} years old
 `;
 
 console.log(template);
 // output:
-//   John Doe
-//            has
+//   some people
+//             has
 //                            21 years old
