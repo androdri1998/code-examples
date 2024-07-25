edb5ddca9db9d0817545568626e9c166b1ad5419
André Rodrigues <andre.rodrigues98@outlook.com>
2024-07-24 22:03:09 -0300

    fix: example of destructuring

--- JavaScript/concepts/destructure-2.js
+++ JavaScript/concepts/destructure-2.js
@@ -1,12 +1,12 @@
 // array
 const arr = [1, 2, 3];
-[one, two, three] = arr;
+const [one, two, three] = arr;
 console.log("array", one, two, three);
 // output: array 1 2 3
 
 // object
 const obj = { first: 1, second: 2, third: 3 };
-const { first, second, nonExistent } = obj;
+let { first, second, nonExistent } = obj;
 console.log("object", first, second, nonExistent);
 // output: object 1 2 undefined
 
